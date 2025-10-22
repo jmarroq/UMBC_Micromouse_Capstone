1e6b2298708fc3635fb46f3573441f8ae25a096e
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2025-10-22 11:21:00 -0400

    Create system_init.c

---
+++ Core/system_init.c
@@ -0,0 +1,9 @@
+// system_init.c
+#include "system_init.h"
+#include <stdio.h>
+
+void System_Init(void) {
+    // In real STM32: HAL_Init(), clock config, etc.
+    printf("System Initialized.\n");
+}
+
