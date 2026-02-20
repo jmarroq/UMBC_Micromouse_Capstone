51a3b3a71598bc05f60ff364c40f52606ffcc075
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2026-02-20 11:34:23 -0500

    Update Stack.h

--- Simulation/Stack.h
+++ Simulation/Stack.h
@@ -1,1 +1,14 @@
 
+#ifndef STACK_H
+#define STACK_H
+
+#include "Maze.h"  
+
+Stack* new_Stack(void);
+void delete_Stack(Stack** spp);
+
+int is_empty_Stack(Stack* this_stack);
+void pop(Stack* this_stack, Node** npp);
+void push(Stack* this_stack, Node* this_node);
+
+#endif
