55300c808514ba6cf0e983b8ef1f63593f1c6b76
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2026-02-20 11:55:01 -0500

    Update Stack.h

--- Simulation/Stack.h
+++ Simulation/Stack.h
@@ -8,7 +8,7 @@ Stack* new_Stack(void);
 void delete_Stack(Stack** spp);
 
 int is_empty_Stack(Stack* this_stack);
-void pop(Stack* this_stack, Node** npp);
-void push(Stack* this_stack, Node* this_node);
+void pop(Stack* this_stack, Cell** npp);
+void push(Stack* this_stack, Cell* this_node);
 
 #endif
