8a45d4e73ed6e22d3b2987b7bdc878e0d8d79b16
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2026-03-09 14:48:00 -0400

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include <Adafruit_VL6180X.h>
 
 struct SensorsFrame {
   uint32_t t_ms;
@@ -19,20 +20,14 @@ public:
   // Establish Mux address0x70 for TCA9548A
   explicit Sensors(uint8_t mux_addr = 0x70);
 
-  // Call once in setup()
   bool begin();
-
-  // Returns true if a new frame was produced this call.
   bool update20ms();
-
-  // Copy out the latest frame (fast, simple).
   void getLatest(SensorsFrame& out) const;
 
 private:
-  bool readOne(uint8_t mux_channel, uint16_t& mm_out, bool& valid_out);
-
   uint8_t _mux_addr;
   SensorsFrame _latest;
-
-  uint32_t _next_due_ms;   // for steady 50 Hz scheduling
+  Adafruit_VL6180X _vl;
+  uint32_t _next_due_ms;
+  bool readOne(uint8_t mux_channel, uint16_t& mm_out, bool& valid_out);
 };
